ccf074d883e2757b1628f2e5a7e562c7aa5b7570
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2023-12-22 15:51:13 +0700

    Create controllerNotModel.php

---
+++ controllerNotModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use DB;
+class AdminComments extends Controller
+{
+    public function index(){
+        $pageTitle = 'Comments';
+        $emptyMessage = 'No data found';
+        $data = DB::table('comments')->get();
+        return view('admin.comments.list', compact('pageTitle','data')); 
+    }
+}
